7a6ba0c964fa7bdc2fda8b49456b8314a0b0b6c9
la-fourier <jojoi7@gmx.de>
2021-11-01 15:21:05 +0100

    storage, neuer griparm

---
+++ hardmodels_jomo.py
@@ -0,0 +1,44 @@
+## 0) Filepath----------------------------------------------------------------------------------------------------------
+##    
+#!/usr/bin/env pybricks-micropython
+
+
+
+## 1) Commentation Zone-------------------------------------------------------------------------------------------------
+##    
+##    Author: Johannes und Moritz
+##    Language: Micropython (Python) v 2.0
+##    Important comments: This program requires LEGO EV3 MicroPython v2.0 or higher.
+##                        Garbage Collector might needed.
+##    File: 
+##         Program ( ) 
+##         Module  (x)
+##    Version: 1.0.13 (Version.release.commit, reset per each new version and release)
+##    Look for the documentation. If there are questions, write to the Support.
+
+
+
+## 2) Import Zone-------------------------------------------------------------------------------------------------------
+##    
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile 
+
+import gc
+
+
+
+## 3) Moduling Zone--------------------------------------------------------------------------------------------------
+##
+
+class Basic_jomo(Customclass_jomo, Robotics_jomo):
+    #fill. Alle Werte so initialisieren wie sie für Basic Drivebase benötigt werden
+
+
+class Griparm_jomo(Customclass_jomo, Extension_jomo):
+    #fill
+#hier noch weitere Spezialisierte Klassen
