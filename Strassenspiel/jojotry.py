4f4ca6ae4fa958712a3dd4252c4c85f93426c923
la fourier <jojoi7@gmx.de>
2021-11-25 09:29:53 +0100

    Update jojotry.py

--- Strassenspiel/jojotry.py
+++ Strassenspiel/jojotry.py
@@ -37,7 +37,7 @@ class Strassengame(EV3Brick):
 
     class Straße(threading.Thread):
 
-        def __init__(self, id="default", port=Port.A, test_=False):
+        def __init__(self, id="default", port=Port.A, test_=False): #FINE
             threading.Thread.__init__(self)
             self.id=id
             self.mot=Motor(port)
@@ -67,7 +67,7 @@ class Strassengame(EV3Brick):
             if self.test_==True:
                 print("Neuer Term zum Hinbeschleunigen wurde erfolgreich geladen. ")
 
-    def __init__(self, test_=False):
+    def __init__(self, test_=False): #FINE
         self.l=Straße(id="l", port=Port.A, test_=test_)
         self.m=Straße(id="m", port=Port.B, test_=test_)
         self.r=Straße(id="r", port=Port.C, test_=test_)
@@ -78,7 +78,7 @@ class Strassengame(EV3Brick):
     def stelleSchnelligkeit(self):
         pass
 
-    def sel_menu_item(self, args=[]):
+    def sel_menu_item(self, args=[]): #FINE
         self.screen.clear()
         emph=0
         i=0
@@ -89,15 +89,28 @@ class Strassengame(EV3Brick):
                 else:
                     self.screen.draw_text(x=15, y=10+15*i, text=arg, text_color=Color.BLACK, background_color=Color.WHITE)
                 i=i+1
-            if (Button.LEFT in self.buttons.pressed()) or (Button.DOWN in self.buttons.pressed()):
+            if (Button.RIGHT in self.buttons.pressed()) or (Button.DOWN in self.buttons.pressed()):
                 emph=(emph+1)%5
-            elif (Button.RIGHT in self.buttons.pressed()) or (Button.UP in self.buttons.pressed()):
+            elif (Button.LEFT in self.buttons.pressed()) or (Button.UP in self.buttons.pressed()):
                 emph=(emph-1)%5
             self.screen.clear()
         return emph
 
     def anleitung(self):
-        pass
+        texte=["Bei diesem Spiel geht es darum, mit dem Auto möglichst lange den Hindernissen auszuweichen.",
+             "Das alleine wäre ja langweilig, doch währrend dem Spiel verändert sich deine Geschwindigkeit!",
+             "Mit der Zeit wechselt die Geschwindigkeit öfter, schneller und zufälliger!",
+             "Durch einen Druck auf die Mittlere Bricktaste kannst du das Spiel unterbrechen, durch einen zweiten Druck beenden."]
+        emph=0
+        while not (Button.CENTER in self.buttons.pressed()):
+            self.screen.clear()
+            self.screen.print(texte[emph])
+            self.screen.print(" [Hoch/Links=Zurück, Runter/Rechts=Vor, Mitte=Ende] ")
+            self.screen.print(emph +" / " +len(texte))
+            if (Button.RIGHT in self.buttons.pressed()) or (Button.DOWN in self.buttons.pressed()):
+                emph=(emph+1)%len(texte)
+            elif (Button.LEFT in self.buttons.pressed()) or (Button.UP in self.buttons.pressed()):
+                emph=(emph-1)%len(texte)
 
     def game(self):
         while not (Button.CENTER in self.buttons.pressed()):
@@ -106,9 +119,10 @@ class Strassengame(EV3Brick):
     def advertisement(self):
         pass                        #optional
 
-    def play(self):
+    def play(self): #FINE
         # Teil 1: Willkommen
-        self.screen.draw_text(x=10, y=10, text="Willkommen! Du willst spielen? Dann bist du hier genau richtig! Drücke den Mittleren Knopf, um fortzufahren. ")
+        self.screen.draw_text(x=10, y=10, 
+        text="Willkommen! Du willst spielen? Dann bist du hier genau richtig! Drücke den Mittleren Knopf, um fortzufahren. ")
         while not (Button.CENTER in self.buttons.pressed()):
             pass
         if self.test_=True:
@@ -134,6 +148,8 @@ class Strassengame(EV3Brick):
             pass
         self.screen.clear()
         wait(2000)
+        if self.test_=True:
+            print("self.game() wird jetzt gestartet. ")
         self.game()
         if self.test_=True:
             print("Teil 4 ist erfolgreich abgelaufen. ")
@@ -144,4 +160,5 @@ class Strassengame(EV3Brick):
 ##    
 game=Strassengame(test_=True)
 game.play()
+
 
