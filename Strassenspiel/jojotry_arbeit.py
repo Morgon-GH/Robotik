8029b4d840e5ec8496f8e970f404bae864ca6ed0
la-fourier <jojoi7@gmx.de>
2021-11-25 19:49:42 +0100

    so fertig, bis weitere hardware details bekannt sind, und für das spiel an sich

--- Strassenspiel/jojotry_arbeit.py
+++ Strassenspiel/jojotry_arbeit.py
@@ -6,7 +6,7 @@
 
 ## 1) Commentation Zone-------------------------------------------------------------------------------------------------
 ##    
-##    Author: Johannes und Moritz
+##    Author: Johannes
 ##    Language: Micropython (Python) v 2.0
 ##    Important comments: This program requires LEGO EV3 MicroPython v2.0 or higher.
 ##                        Garbage Collector might needed.
@@ -28,6 +28,8 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 import threading
 
+import time
+
 
 
 ## 3) Definition Zone---------------------------------------------------------------------------------------------------
@@ -37,48 +39,65 @@ class Strassengame(EV3Brick):
 
     class Straße(threading.Thread):
 
-        def __init__(self, id="default", port=Port.A, test_=False): #FINE
+        def __init__(self, id="default", port=Port.A, test_=False): ################################################################################################FINE
             threading.Thread.__init__(self)
+            if type(port)!=String:
+                self.mot=Motor(port)
+            else:
+                pass
             self.id=id
-            self.mot=Motor(port)
-            self.tempo_=True
             self.tempo=100
             self.hinbeschleunigen={1: 1}
             self.test_=test_
+            self.run_=True
             if self.test_==True:
                 print("Straße ist betriebsbereit. ")
 
         def beschleunige_hin(self):
-            pass
+            if self.mot.speed>self.tempo:
+                while self.mot.speed!=self.tempo:
+                pass
+            elif self.mot.speed<self.tempo:
+                while self.mot.speed!=self.tempo:
+                pass
+            else:
+                pass
+
+            if self.test_==True:
+                print("Neuer Term zum Hinbeschleunigen wurde erfolgreich geladen. ")
         
         def run(self):
             if self.test_==True:
                 print("Thread wurde gestartet, in Kürze wird die Straße wahrscheinlich anfangen zu laufen")
 
-            pass
+            while self.run_=True:
+                pass
 
             if self.test_==True:
                 print("Thread wurde beendet. Die Straße wird nun in diesem Thread nicht mehr laufen. ")
 
-        def sethinbeschleunigen(self):
+        def run_(self, level=0):
+            while self.run_=True:
+                pass
 
-            pass
+        
 
-            if self.test_==True:
-                print("Neuer Term zum Hinbeschleunigen wurde erfolgreich geladen. ")
-
-    def __init__(self, test_=False): #FINE
+    def __init__(self, test_=False): ######################################################################################################################FINE
         self.l=Straße(id="l", port=Port.A, test_=test_)
         self.m=Straße(id="m", port=Port.B, test_=test_)
         self.r=Straße(id="r", port=Port.C, test_=test_)
+        self.timemanager=Straße(id="t", port="None", test_=test_)
         self.test_=test_
         if self.test_==True:
             print("Straßengame wurde betriebsfertig gemacht. ")
 
-    def stelleSchnelligkeit(self):
+    def stelleBeschleunigen(self, level=0):
         pass
 
-    def sel_menu_item(self, args=[]): #FINE
+    def stelleZielgeschwindigkeiten(self, level=0):
+        pass
+    
+    def sel_menu_item(self, args=[]): ######################################################################################################################FINE
         self.screen.clear()
         emph=0
         i=0
@@ -96,11 +115,12 @@ class Strassengame(EV3Brick):
             self.screen.clear()
         return emph
 
-    def anleitung(self):
+    def anleitung(self): ######################################################################################################################FINE
         texte=["Bei diesem Spiel geht es darum, mit dem Auto möglichst lange den Hindernissen auszuweichen.",
              "Das alleine wäre ja langweilig, doch währrend dem Spiel verändert sich deine Geschwindigkeit!",
              "Mit der Zeit wechselt die Geschwindigkeit öfter, schneller und zufälliger!",
-             "Durch einen Druck auf die Mittlere Bricktaste kannst du das Spiel unterbrechen, durch einen zweiten Druck beenden."]
+             "Durch einen Druck auf die Mittlere Bricktaste kannst du das Spiel unterbrechen, durch einen zweiten Druck beenden."
+             "Lenken kannst du mit dem Lenkrad, aber jetzt viel Spaß dir!"]
         emph=0
         while not (Button.CENTER in self.buttons.pressed()):
             self.screen.clear()
@@ -112,14 +132,20 @@ class Strassengame(EV3Brick):
             elif (Button.LEFT in self.buttons.pressed()) or (Button.UP in self.buttons.pressed()):
                 emph=(emph-1)%len(texte)
 
-    def game(self):
+    def game(self, level=0):
+        self.timemanager.run_(level=level)
+        self.l.run()
+        self.r.run()
+        self.m.run()
+        global starttime=time.perf_counter()
         while not (Button.CENTER in self.buttons.pressed()):
-            pass
+            self.screen.clear()
+            self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
 
     def advertisement(self):
         pass                        #optional
 
-    def play(self): #FINE
+    def play(self): ######################################################################################################################FINE
         # Teil 1: Willkommen
         self.screen.draw_text(x=10, y=10, 
         text="Willkommen! Du willst spielen? Dann bist du hier genau richtig! Drücke den Mittleren Knopf, um fortzufahren. ")
@@ -150,14 +176,14 @@ class Strassengame(EV3Brick):
         wait(2000)
         if self.test_=True:
             print("self.game() wird jetzt gestartet. ")
-        self.game()
+        self.game(level=level)
         if self.test_=True:
             print("Teil 4 ist erfolgreich abgelaufen. ")
 
 
 
 ## 4) Programming Zone--------------------------------------------------------------------------------------------------
-##    
+##   
 game=Strassengame(test_=True)
 game.play()
 
