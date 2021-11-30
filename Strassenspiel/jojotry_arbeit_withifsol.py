bebf6ffceef597539cb172cd3490efdc1bf64058
la fourier <jojoi7@gmx.de>
2021-11-30 14:50:58 +0100

    Update jojotry_arbeit_withifsol.py

--- Strassenspiel/jojotry_arbeit_withifsol.py
+++ Strassenspiel/jojotry_arbeit_withifsol.py
@@ -13,6 +13,8 @@
 ##    File: 
 ##         Program (x) 
 ##         Module  ( )
+##    Comments: Hier wurde alles in die play()-Methode ausgelagert, im Gegensatz zur thread-Lösung, wo noch eine game()-Methode
+##               investiert wurde. Evt könnte nach Teil 1 in play() noch ein Teil für Spracherkennung investiert werden.
 
 
 
@@ -47,22 +49,24 @@ class Strassengame(EV3Brick):
                 self.mot=Motor(port)
             else:
                 pass
-            self.id=id
-            self.tempo=100
-            self.hinbeschleunigen={}
-            self.test_=test_
-            self.run_=True
-            self.end_=False
+            self.id=id #um Thread 2Fakt authentifizieren zu können
+            self.tempo=100 #Geschwindigkeit des zugehörigen Motors
+            self.hinbeschleunigen={} #koeffizienten für polynomielle Beschleunigung
+            self.test_=test_ #für print-Ausgaben, um den Ablauf zu überprüfen
+            self.run_=True #variable, ob gerade der Motor laufen soll
+            self.end_=False #variable, ob beendet werden soll; vorher muss self.run_ auf False gesetzt werden!
             if self.test_==True:
                 print("Straße ist betriebsbereit. ")
 
         def beschleunige_hin(self):
             if self.mot.speed>self.tempo:
-                while self.mot.speed!=self.tempo:
-                pass
+                while self.mot.speed()!=self.tempo:
+                    if self.run_==False:
+                        self.mot.hold()
             elif self.mot.speed<self.tempo:
-                while self.mot.speed!=self.tempo:
-                pass
+                while self.mot.speed()!=self.tempo:
+                    if self.run_==False:
+                        self.mot.hold()
             else:
                 pass
 
@@ -132,86 +136,14 @@ class Strassengame(EV3Brick):
             elif (Button.LEFT in self.buttons.pressed()) or (Button.UP in self.buttons.pressed()):
                 emph=(emph-1)%len(texte)
 
-    def game(self, level=0):
-        #WAS BEDEUTET WELCHES LEVEL?
-        # Schwierigkeitsstufe    | Beschleunigen                                                                      | Zielgeschwindigkeit
-        # Leicht(0)              | Pol vom Grad 2;              wird per randint am Anfang festgelegt, dann konstant  | Variiert alle 5 Sekunden
-        # Eher Leicht(1)         | Pol vom Grad 3;              wird per randint am Anfang festgelegt, dann konstant  | Variiert alle randint(4, 8) Sekunden
-        # Mittel(2)              | Pol vom Grad randint(2, 5);  Variiert alle 5 Sekunden                              | Variiert alle randint(3, 7) Sekunden
-        # Eher Schwer(3)         | Pol vom Grad randint(2, 6);  Variiert alle randint(4, 8) Sek                       | Variiert alle randint(2, 6) Sekunden
-        # Schwer(4)              | Pol vom Grad randint(3, 6);  Variiert alle randint(3, 7) Sek                       | Variiert alle randint(1, 5) Sekunden
-        # ----------------------------------------------------------------------------------------------------------------------------------------------
-        # 5 Schwierigkeiten      |  Polynomiell;                 fest/(variabel) variierend                           | (variabel) variierend              (RANDINT-variator)
+    def poly_beschl(self, min_polgr=10, max_polgr=0):
+        polgr=ranom.randint(min_polgr, max_polgr)
+        for koe in range(0, polgr):
+            self.l.hinbeschleunigen[koe]=random.randint(1, 10)
+            self.m.hinbeschleunigen[koe]=random.randint(1, 10)
+            self.r.hinbeschleunigen[koe]=random.randint(1, 10)
 
-        self.l.run()
-        self.r.run()
-        self.m.run()
-        if level==0:
-            tempo=5
-            tempo_=0
-            self.l.hinbeschleunigen[0]=random.randint(1, 10); self.l.hinbeschleunigen[1]=random.randint(1, 10); self.l.hinbeschleunigen[2]=random.randint(1, 10)
-            self.m.hinbeschleunigen[0]=random.randint(1, 10); self.m.hinbeschleunigen[1]=random.randint(1, 10); self.m.hinbeschleunigen[2]=random.randint(1, 10)
-            self.r.hinbeschleunigen[0]=random.randint(1, 10); self.r.hinbeschleunigen[1]=random.randint(1, 10); self.r.hinbeschleunigen[2]=random.randint(1, 10)
-            self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200)
-            global starttime=time.perf_counter()
-            while not (Button.CENTER in self.buttons.pressed()):
-                self.screen.clear()
-                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
-                if (int(time.perf_counter()-starttime)>(tempo*tempo_)):
-                    self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200); tempo_=tempo_+1
-        
-        elif level==1:
-            tempo=0
-            self.l.hinbeschleunigen[0]=random.randint(1, 10); self.l.hinbeschleunigen[1]=random.randint(1, 10); self.l.hinbeschleunigen[2]=random.randint(1, 10)
-            self.l.hinbeschleunigen[3]=random.randint(1, 10)
-            self.m.hinbeschleunigen[0]=random.randint(1, 10); self.m.hinbeschleunigen[1]=random.randint(1, 10); self.m.hinbeschleunigen[2]=random.randint(1, 10)
-            self.m.hinbeschleunigen[3]=random.randint(1, 10)
-            self.r.hinbeschleunigen[0]=random.randint(1, 10); self.r.hinbeschleunigen[1]=random.randint(1, 10); self.r.hinbeschleunigen[2]=random.randint(1, 10)
-            self.r.hinbeschleunigen[3]=random.randint(1, 10)
-            global starttime=time.perf_counter()
-            while not (Button.CENTER in self.buttons.pressed()):
-                self.screen.clear()
-                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
-                if (int(time.perf_counter()-starttime)>(tempo)):
-                    self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200); tempo=randint(4, 8)+int(time.perf_counter()-starttime)
-        
-        elif level==2:
-            beschl=1
-            tempo=1
-            beschl_=0
-            self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200)
-            global starttime=time.perf_counter()
-            while not (Button.CENTER in self.buttons.pressed()):
-                self.screen.clear()
-                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
-                if (int(time.perf_counter()-starttime)>(tempo)):
-                    self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200); tempo=randint(3, 7)+int(time.perf_counter()-starttime)
-                elif (int(time.perf_counter()-starttime)>(tempo*tempo_)):
-                    pass
-        
-        elif level==3:
-            beschl=1
-            tempo=1
-            global starttime=time.perf_counter()
-            while not (Button.CENTER in self.buttons.pressed()):
-                self.screen.clear()
-                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
-        
-        elif level==4:
-            beschl=1
-            temp=1
-            global starttime=time.perf_counter()
-            while not (Button.CENTER in self.buttons.pressed()):
-                self.screen.clear()
-                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
-        
-        else:
-            pass
-
-    def advertisement(self):
-        pass                        #optional
-
-    def play(self): ######################################################################################################################FINE
+    def play(self):
         # Teil 1: Willkommen
         self.screen.draw_text(x=10, y=10, 
         text="Willkommen! Du willst spielen? Dann bist du hier genau richtig! Drücke den Mittleren Knopf, um fortzufahren. ")
@@ -235,17 +167,122 @@ class Strassengame(EV3Brick):
         if self.test_=True:
             print("Teil 3 ist erfolgreich abgelaufen. ")
 
-        # Teil 4: CENTER-Abfrage und Spiel
+        # Teil 4: CENTER-Abfrage und Spielstart
         self.screen.draw_text(x=10, y=10, text="Viel Spaß! Drücke den Mittleren Knopf, um fortzufahren. ")
         while not (Button.CENTER in self.buttons.pressed()):
             pass
         self.screen.clear()
         wait(2000)
         if self.test_=True:
-            print("self.game() wird jetzt gestartet. ")
+            print("Spielvorgang wird jetzt gestartet. ")
         self.game(level=level)
+
+        # Teil 5: Fallunterscheidung zwischen Level; vllt Auslagerung in eigene Methoden?
+        #WAS BEDEUTET WELCHES LEVEL?
+        # Schwierigkeitsstufe    | Beschleunigen                                                                      | Zielgeschwindigkeit
+        # Leicht(0)              | Pol vom Grad 2;              wird per randint am Anfang festgelegt, dann konstant  | Variiert alle 5 Sekunden
+        # Eher Leicht(1)         | Pol vom Grad 3;              wird per randint am Anfang festgelegt, dann konstant  | Variiert alle randint(4, 8) Sekunden
+        # Mittel(2)              | Pol vom Grad randint(2, 5);  Variiert alle 5 Sekunden                              | Variiert alle randint(3, 7) Sekunden
+        # Eher Schwer(3)         | Pol vom Grad randint(2, 6);  Variiert alle randint(4, 8) Sek                       | Variiert alle randint(2, 6) Sekunden
+        # Schwer(4)              | Pol vom Grad randint(3, 6);  Variiert alle randint(3, 7) Sek                       | Variiert alle randint(1, 5) Sekunden
+        # ----------------------------------------------------------------------------------------------------------------------------------------------
+        # 5 Schwierigkeiten      |  Polynomiell;                 fest/(variabel) variierend; KoEs zwischen 1 und 10   | (variabel) variierend; zwischen 1 und 200       (RANDINT-variator)
+
         if self.test_=True:
-            print("Teil 4 ist erfolgreich abgelaufen. ")
+            print("Teil 5 ist erfolgreich gestartet. ")
+        self.l.run()
+        self.r.run()
+        self.m.run()
+        if level==0:
+            tempo=5
+            tempo_=1
+            self.l.hinbeschleunigen[0]=random.randint(1, 10)
+            self.l.hinbeschleunigen[1]=random.randint(1, 10)
+            self.l.hinbeschleunigen[2]=random.randint(1, 10)
+            self.m.hinbeschleunigen[0]=random.randint(1, 10)
+            self.m.hinbeschleunigen[1]=random.randint(1, 10)
+            self.m.hinbeschleunigen[2]=random.randint(1, 10)
+            self.r.hinbeschleunigen[0]=random.randint(1, 10)
+            self.r.hinbeschleunigen[1]=random.randint(1, 10)
+            self.r.hinbeschleunigen[2]=random.randint(1, 10)
+            self.l.tempo=random(1, 200)
+            self.m.tempo=random(1, 200)
+            self.r.tempo=random(1, 200)
+            global starttime=time.perf_counter()
+            while not (Button.CENTER in self.buttons.pressed()):
+                self.screen.clear()
+                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
+                self.screen.print("[Mitte=Unterbrechung]")
+                if (int(time.perf_counter()-starttime)>(tempo*tempo_)):
+                    self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200)
+                    tempo_=tempo_+1
+        
+        elif level==1:
+            tempo=0
+            self.l.hinbeschleunigen[0]=random.randint(1, 10)
+            self.l.hinbeschleunigen[1]=random.randint(1, 10)
+            self.l.hinbeschleunigen[2]=random.randint(1, 10)
+            self.l.hinbeschleunigen[3]=random.randint(1, 10)
+            self.m.hinbeschleunigen[0]=random.randint(1, 10)
+            self.m.hinbeschleunigen[1]=random.randint(1, 10)
+            self.m.hinbeschleunigen[2]=random.randint(1, 10)
+            self.m.hinbeschleunigen[3]=random.randint(1, 10)
+            self.r.hinbeschleunigen[0]=random.randint(1, 10)
+            self.r.hinbeschleunigen[1]=random.randint(1, 10)
+            self.r.hinbeschleunigen[2]=random.randint(1, 10)
+            self.r.hinbeschleunigen[3]=random.randint(1, 10)
+            global starttime=time.perf_counter()
+            while not (Button.CENTER in self.buttons.pressed()):
+                self.screen.clear()
+                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
+                self.screen.print("[Mitte=Unterbrechung]")
+                if (int(time.perf_counter()-starttime)>(tempo)):
+                    self.l.tempo=random(1, 200); self.m.tempo=random(1, 200); self.r.tempo=random(1, 200); tempo=randint(4, 8)+int(time.perf_counter()-starttime)
+        
+        elif level==2:
+            beschl=1
+            tempo=5
+            beschl_=1
+            self.l.tempo=random(1, 200)
+            self.m.tempo=random(1, 200)
+            self.r.tempo=random(1, 200)
+            self.poly_beschl(min_polgr=2, max_polgr=5)
+            global starttime=time.perf_counter()
+            while not (Button.CENTER in self.buttons.pressed()):
+                self.screen.clear()
+                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
+                self.screen.print("[Mitte=Unterbrechung]")
+                if (int(time.perf_counter()-starttime)>(tempo)):
+                    self.l.tempo=random(1, 200)
+                    self.m.tempo=random(1, 200)
+                    self.r.tempo=random(1, 200)
+                    tempo=randint(3, 7)+int(time.perf_counter()-starttime)
+                elif (int(time.perf_counter()-starttime)>(beschl*beschl_)):
+                    self.poly_beschl(min_polgr=2, max_polgr=5)
+                    beschl_=beschl_+1
+        
+        elif level==3:  #hier
+            beschl=1
+            tempo=1
+            global starttime=time.perf_counter()
+            while not (Button.CENTER in self.buttons.pressed()):
+                self.screen.clear()
+                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
+                self.screen.print("[Mitte=Unterbrechung]")
+        
+        elif level==4:
+            beschl=1
+            temp=1
+            global starttime=time.perf_counter()
+            while not (Button.CENTER in self.buttons.pressed()):
+                self.screen.clear()
+                self.screen.print("Dein Score ist: " +str(int((time.perf_counter()-starttime)*100)))
+                self.screen.print("[Mitte=Unterbrechung]")
+        
+        else:
+            pass
+        if self.test_=True:
+            print("Teil 5 ist erfolgreich abgelaufen. ")
 
 
 
